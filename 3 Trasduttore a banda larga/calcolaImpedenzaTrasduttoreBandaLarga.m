0e4e89e9ef07e7ca745818e604197235b9084724
Simone Smaldore <>
2023-01-12 11:21:15 +0100

    ß Corretta matrice T del trasduttore a banda larga

--- 3 Trasduttore a banda larga/calcolaImpedenzaTrasduttoreBandaLarga.m
+++ 3 Trasduttore a banda larga/calcolaImpedenzaTrasduttoreBandaLarga.m
@@ -9,13 +9,13 @@ function [Zin, FTT] = calcolaImpedenzaTrasduttoreBandaLarga(freq, areaPiezo, z_c
     % Matrice T (Simile a matrice G 2 ceramiche però accoppia ceramica e massa)
     T(1,1) = A(1,1)-((A(1,2)^2/(A(1,1)+M11)));
     T(1,2) = A(1,2)*M12/(A(1,1)+M11);
-    T(1,3) = A(1,3)-A(1,2)*A(1,3)/(A(1,1)+M11);
-    T(2,1) = M12*A(1,2)/(A(1,1)+M11);
+    T(1,3) = (A(1,3)-A(1,2)*A(1,3)/(A(1,1)+M11))/2;
+    T(2,1) = T(1,2);
     T(2,2) = M11-(M12^2/(A(1,1)+M11));
-    T(2,3) = A(1,3)*M12/(A(1,1)+M11);
-    T(3,1) = A(1,3)-(A(1,3)*A(1,2))/(A(1,1)+M11);
-    T(3,2) = A(1,3)*M12/(A(1,1)+M11);
-    T(3,3) = A(3,3)-(A(1,3)^2)/(A(1,1)+M11);
+    T(2,3) = (A(1,3)*M12/(A(1,1)+M11))/2;
+    T(3,1) = T(1,3);
+    T(3,2) = T(2,3);
+    T(3,3) = (A(3,3)/2-(A(1,3)^2)/(2*(A(1,1)+M11)))/2;
         
     B = calcolaMatriceB(T, Z1);
 
