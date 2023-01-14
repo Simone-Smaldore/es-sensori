f1f6d31f05f337d0f0b1a2b8c96eb16204adf747
Simone Smaldore <>
2023-01-14 09:25:47 +0100

    ß Aggiunta funzione per il calcolo del keff

---
+++ condivise/calcolaKeff.m
@@ -0,0 +1,9 @@
+function [Keff] = calcolaKeff(Zin, freq_vector)
+    [val_min,index_min] = min(abs(Zin));
+    [val_max,index_max] = max(abs(Zin));
+    f_s = freq_vector(index_min);
+    f_p = freq_vector(index_max);
+    Keff2 = (f_p^2 - f_s^2)/ f_s^2;
+    Keff = sqrt(Keff2);
+end
+
